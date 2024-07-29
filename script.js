57018f559689c19e87f927a4792d270c9e378dc8
FilmCat-Web <davidzosa98@gmail.com>
2024-07-29 01:16:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const jsonUrl = 'https://filmcatdb-default-rtdb.firebaseio.com/list/list_movie_serie/.json';
+
+fetch(jsonUrl)
+    .then(response => response.json())
+    .then(data => {
+        const moviesContainer = document.getElementById('movies');
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                const movie = data[key];
+                const movieElement = document.createElement('div');
+                movieElement.className = 'movie';
+                movieElement.innerHTML = `
+                    <img src="${movie.img_poster || 'default-image.jpg'}" alt="${movie.title || 'Título desconocido'}">
+                    <div class="movie-title"><span>${movie.title || 'Título desconocido'}</span></div>
+                `;
+                moviesContainer.appendChild(movieElement);
+            }
+        }
+    })
+    .catch(error => console.error('Error al cargar los datos:', error));
